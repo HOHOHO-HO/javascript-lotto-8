43d7e1c407337d59f5622162aa2bfff193273464
HoCHoRoo <rttkhc0304@gmail.com>
2025-11-02 22:28:04 +0900

    feat: App.js에 로또 게임 적용

--- src/App.js
+++ src/App.js
@@ -1,5 +1,45 @@
+import { OutputView } from "./view/OutputView.js";
+import LottoManager from "./model/LottoManager.js";
+import StatisticsService from "./service/StatisticsService.js";
+import { Console } from "@woowacourse/mission-utils";
+import { InputView } from "./view/InputView.js";
+
 class App {
-  async run() {}
+  async run() {
+    try {
+      const amount = await this.#getAmount();
+      const lottoManager = new LottoManager(amount);
+      const lottos = lottoManager.getLottos();
+
+      OutputView.printLottoCount(lottoManager.getCount());
+      OutputView.printLottos(lottos);
+
+      const winningNumbers = await this.#getWinningNumbers();
+      const bonusNumber = await this.#getBonusNumber();
+
+      const statistics = new StatisticsService();
+      statistics.calculateStatistics(lottos, winningNumbers, bonusNumber);
+
+      OutputView.printStatistics(statistics.getResults());
+      const roi = statistics.calculateROI(amount);
+      OutputView.printROI(roi);
+    } catch (error) {
+      Console.print(error.message);
+      this.run();
+    }
+  }
+
+  async #getAmount() {
+    return InputView.inputPurchaseAmount();
+  }
+
+  async #getWinningNumbers() {
+    return InputView.inputWinningNumbers();
+  }
+
+  async #getBonusNumber() {
+    return InputView.inputBonusNumber();
+  }
 }
 
 export default App;
