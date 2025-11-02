6d3730f148594c72e25292f48d8a268d8a358582
HoCHoRoo <rttkhc0304@gmail.com>
2025-11-02 22:28:24 +0900

    test: 로또 클래스 관련 테스트 추가

---
+++ __tests__/model/LottoTest.js
@@ -0,0 +1,21 @@
+import Lotto from "../../src/model/Lotto.js";
+
+describe("로또 클래스 테스트", () => {
+  test("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
+    expect(() => {
+      new Lotto([1, 2, 3, 4, 5, 6, 7]);
+    }).toThrow("[ERROR]");
+  });
+
+  test("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.", () => {
+    expect(() => {
+      new Lotto([1, 2, 3, 4, 5, 5]);
+    }).toThrow("[ERROR]");
+  });
+
+  test("로또 번호에 숫자 이외의 값이 있다면 예외가 발생한다.", () => {
+    expect(() => {
+      new Lotto(["1", "d", 3, 7, 8, 9]);
+    }).toThrow("[ERROR]");
+  });
+});
