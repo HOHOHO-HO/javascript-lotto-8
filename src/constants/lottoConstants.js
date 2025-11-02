088a1c386ea932f6a14cd817320a7d1138a03466
HoCHoRoo <rttkhc0304@gmail.com>
2025-11-02 22:26:05 +0900

    feat: 로또 번호 관련 상수 추가

---
+++ src/constants/lottoConstants.js
@@ -0,0 +1,22 @@
+export const LOTTO = {
+  PRICE: 1000,
+  NUMBER_COUNT: 6,
+  MIN_NUMBER: 1,
+  MAX_NUMBER: 45,
+};
+
+export const RANK = {
+  FIRST: 1,
+  SECOND: 2,
+  THIRD: 3,
+  FOURTH: 4,
+  FIFTH: 5,
+};
+
+export const PRIZE = {
+  [RANK.FIRST]: 2_000_000_000,
+  [RANK.SECOND]: 30_000_000,
+  [RANK.THIRD]: 1_500_000,
+  [RANK.FOURTH]: 50_000,
+  [RANK.FIFTH]: 5_000,
+};
